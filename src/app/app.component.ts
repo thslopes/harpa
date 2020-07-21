d99ef4b197766d564058d3efcfc7623e3e5e5394
Thiago Lopes <thiago.slopes@b2wdigital.com>
2020-07-21 02:45:59 -0300

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { hinos } from './hinos'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'harpa';
+  hinos = {};
+  hino = null
+  nome = ""
+  letra = ""
+  selected = 0;
+
+  constructor() {
+    hinos.forEach(hino => {
+      this.hinos[hino.id] = hino;
+    });
+  }
+
+  public add(val: number): void {
+    this.selected = this.selected * 10 + val;
+    this.nome = this.hinos[this.selected].nome.toUpperCase();
+  }
+
+  public clear(val: Number): void {
+    this.selected = 0;
+    this.hino = this.hinos[this.selected]
+    this.nome = "";
+  }
+
+  public show(): void {
+    this.hino = this.hinos[this.selected]
+    this.nome = this.hino.nome.toUpperCase()
+    this.letra = this.hino.letra
+  }
 }
