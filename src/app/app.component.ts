2a126c69152c1e533d844e07c2a2d4540f3b23ec
Thiago Lopes <thslopes@gmail.com>
2022-05-31 23:48:40 -0300

    show first lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   nome = ""
   letra = ""
   selected = 0;
+  showingAll = false;
 
   constructor() {
     hinos.forEach(hino => {
@@ -21,19 +22,31 @@ export class AppComponent {
   }
 
   public add(val: number): void {
+    this.showingAll = false;
     this.selected = this.selected * 10 + val;
     this.nome = this.hinos[this.selected].nome.toUpperCase();
   }
 
   public clear(val: Number): void {
     this.selected = 0;
-    this.hino = this.hinos[this.selected]
+    this.hino = this.hinos[this.selected];
     this.nome = "";
+    this.showingAll = false;
   }
 
   public show(): void {
-    this.hino = this.hinos[this.selected]
-    this.nome = this.hino.nome.toUpperCase()
-    this.letra = this.hino.letra
+    this.hino = this.hinos[this.selected];
+    this.nome = this.hino.nome.toUpperCase();
+    this.letra = this.hino.letra;
+    this.showingAll = false;
+  }
+
+  public showAll(): void {
+    this.showingAll = true;
+  }
+
+  public select(index: Number): void {
+    this.selected = index;
+    this.show();
   }
 }
