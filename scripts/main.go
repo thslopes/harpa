3bdfeb163ce825e50c8ecbe7480a0d31912b6cf2
Thiago Lopes <thslopes@gmail.com>
2022-06-06 15:12:57 -0300

    search field

--- scripts/main.go
+++ scripts/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/microcosm-cc/bluemonday"
@@ -21,15 +22,20 @@ type hino struct {
 	Nome  string
 	Letra string
 	Linha string
+	Busca string
 }
 
 func (h hino) String() string {
-	// return fmt.Sprintf("%d %s\n", h.Id, h.Linha)
-	return fmt.Sprintf("\n{\"id\": %d,\"nome\": \"%s\",\n\"letra\": \"%s\",\n\"linha\": \"%s\"},", h.Id, h.Nome, strings.ReplaceAll(h.Letra, "\"", "\\\""), strings.ReplaceAll(h.Linha, "\"", "\\\""))
+	// return fmt.Sprintf("%d %s\n", h.Id, h.Busca)
+	return fmt.Sprintf("\n{\"id\": %d,\"nome\": \"%s\",\n\"letra\": \"%s\",\n\"linha\": \"%s\",\n\"busca\": \"%s\"},",
+		h.Id, h.Nome,
+		strings.ReplaceAll(h.Letra, "\"", "\\\""),
+		strings.ReplaceAll(h.Linha, "\"", "\\\""),
+		strings.ReplaceAll(h.Busca, "\"", "\\\""))
 }
 
 func main() {
-	dat, err := os.ReadFile("../src/app/hinos.ts")
+	dat, err := os.ReadFile("hinos.ts")
 	check(err)
 
 	var hinos []hino
@@ -38,22 +44,32 @@ func main() {
 	err = json.Unmarshal(dat, &hinos)
 	check(err)
 
+	reAccents := regexp.MustCompile(`&(\w)\w+;`)
+	reMarks := regexp.MustCompile(`[^\w\s]`)
+
 	for i := range hinos {
 		linha := hinos[i].Letra
 		linha = strings.ReplaceAll(linha, "<br />", " ")
 		linha = strings.ReplaceAll(linha, "&nbsp;", " ")
+		linha = reAccents.ReplaceAllString(linha, "$1")
+
 		linha = p.Sanitize(linha)
+		// linha = strings.ReplaceAll(linha, "&#34;", "")
+		// linha = strings.ReplaceAll(linha, "&#39;", "")
+		linha = reMarks.ReplaceAllString(linha, " ")
 		linha = strings.ReplaceAll(linha, "  ", " ")
-		if len(linha) > 50 {
-			for i := 70; i > 0; i-- {
-				if linha[i] == ' ' {
-					linha = linha[0:i] + "..."
-					break
-				}
-			}
-		}
-		hinos[i].Linha = linha
+		// if len(linha) > 50 {
+		// 		for i := 70; i > 0; i-- {
+		// 			if linha[i] == ' ' {
+		// 				linha = linha[0:i] + "..."
+		// 				break
+		// 			}
+		// 		}
+		// 	}
+		hinos[i].Busca = strings.ToLower(linha)
 	}
+
+	fmt.Println(hinos[0:10])
 
 	dat = []byte(fmt.Sprint(hinos))
 
