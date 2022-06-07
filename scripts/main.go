abe300fb2a1b3b22a54a13ee02bdb95fcdd3dcd8
Thiago Lopes <thslopes@gmail.com>
2022-06-07 13:45:12 -0300

    20023 init

--- scripts/main.go
+++ scripts/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/microcosm-cc/bluemonday"
 )
@@ -29,6 +30,10 @@ func (h hino) String() string {
 }
 
 func main() {
+	if len(os.Args) > 1 {
+		formatEBD()
+		return
+	}
 	dat, err := os.ReadFile("../src/app/hinos.ts")
 	check(err)
 
@@ -58,4 +63,48 @@ func main() {
 	dat = []byte(fmt.Sprint(hinos))
 
 	_ = ioutil.WriteFile("hinos.json", dat, 0644)
+}
+
+func formatEBD() {
+	// https://escolabiblicadominical.com.br/licao-11-sendo-cautelosos-nas-opressoes/
+	dat, err := os.ReadFile("ebd20223/index.html")
+	check(err)
+	htmlContent := string(dat)
+	p := bluemonday.StripTagsPolicy()
+	htmlContent = p.Sanitize(htmlContent)
+	htmlContent = strings.ReplaceAll(htmlContent, "\t", "")
+	for strings.Contains(htmlContent, "  ") {
+		htmlContent = strings.ReplaceAll(htmlContent, "  ", " ")
+	}
+	for strings.Contains(htmlContent, "\n\n") {
+		htmlContent = strings.ReplaceAll(htmlContent, "\n\n", "\n")
+	}
+	i := 0
+	linhas := strings.Split(htmlContent, "\n")
+	for !strings.HasPrefix(linhas[i], "EBD – Lição ") {
+		i++
+	}
+	out := getTitle(linhas[i], 1)
+	for linhas[i] != "TEXTO ÁUREO" {
+		i++
+	}
+
+	for linhas[i] != "SAIBA TUDO SOBRE A ESCOLA DOMINICAL:" {
+		out = append(out, linhas[i])
+		i++
+	}
+
+	fmt.Println(strings.Join(out, "\n"))
+}
+
+func getTitle(s string, n int) []string {
+	meses := []string{"", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"}
+	s = strings.Replace(s, "EBD – ", "", 1)
+	x := strings.Split(s, ": ")
+	num := x[0]
+	title := strings.Split(x[1], " |")[0]
+	titleDate := time.Date(2022, 7, 3, 0, 0, 0, 0, time.UTC)
+	titleDate = titleDate.AddDate(0, 0, 7*(n-1))
+	tdf := fmt.Sprintf(titleDate.Format("2 %s 2006"), meses[titleDate.Month()])
+	return []string{num, tdf, title}
 }
