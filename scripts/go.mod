6bb6ccfec5649d69b0789b618696699599863a69
Thiago Lopes <thslopes@gmail.com>
2022-06-23 12:47:32 -0300

    epub

--- scripts/go.mod
+++ scripts/go.mod
@@ -2,10 +2,17 @@ module github.com/thslopes/harpa
 
 go 1.18
 
-require github.com/microcosm-cc/bluemonday v1.0.18
+require (
+	github.com/bmaupin/go-epub v1.0.0
+	github.com/bmaupin/go-htmlutil v0.0.0-20160828203923-a9558777bf4e
+	github.com/microcosm-cc/bluemonday v1.0.18
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+)
 
 require (
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.3.1 // indirect
+	github.com/gofrs/uuid v3.1.0+incompatible // indirect
 	github.com/gorilla/css v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50 // indirect
 )
--- scripts/main.go
+++ scripts/main.go
@@ -8,6 +8,24 @@ import (
 	"strings"
 
 	"github.com/microcosm-cc/bluemonday"
+
+	"log"
+
+	"golang.org/x/net/html"
+
+	"github.com/bmaupin/go-epub"
+	"github.com/bmaupin/go-htmlutil"
+)
+
+const (
+	effectiveGoCoverImg      = "assets/covers/capa.png"
+	effectiveGoFilename      = "lbap20223.epub"
+	effectiveGoSectionTag    = "h1"
+	effectiveGoTitle         = "Lições Bĩblicas Professor 2022-3"
+	effectiveGoTitleFilename = "title.xhtml"
+	revistaUrl               = "assets/full.html"
+	epubCSSFile              = "assets/ebub.css"
+	preFontFile              = "assets/fonts/SourceCodePro-Regular.ttf"
 )
 
 func check(e error) {
@@ -29,6 +47,15 @@ func (h hino) String() string {
 }
 
 func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "epub" {
+		err := buildEffectiveGo()
+		if err != nil {
+			log.Printf("Error building Effective Go: %s", err)
+		}
+		return
+	}
+
 	if len(os.Args) > 1 {
 		formatEBD()
 		return
@@ -89,4 +116,142 @@ func lessonFile(i int, lines []string) {
 		fileName = "ebd20223/README.md"
 	}
 	_ = ioutil.WriteFile(fileName, dat, 0644)
+}
+
+type epubSection struct {
+	title    string
+	filename string
+	nodes    []html.Node
+}
+
+func buildEffectiveGo() error {
+	r, err := os.Open(revistaUrl)
+	if err != nil {
+		return err
+	}
+	// defer func() {
+	// if err := resp.Body.Close(); err != nil {
+	// panic(err)
+	// }
+	// }()
+
+	doc, err := html.Parse(r)
+	if err != nil {
+		log.Fatalf("Parse error: %s", err)
+	}
+
+	pageNode := htmlutil.GetFirstHtmlNode(doc, "div", "", "")
+
+	sections := []epubSection{}
+	sectionFilename := effectiveGoTitleFilename
+	// Don't add a title so it doesn't get added to the TOC
+	section := &epubSection{
+		filename: effectiveGoTitleFilename,
+	}
+	internalLinks := make(map[string]string)
+
+	initFound := false
+
+	// Iterate through each child node
+	for node := pageNode.FirstChild; node != nil; node = node.NextSibling {
+		if !initFound && getId(node) != "lições-bíblicas" {
+			continue
+		}
+		initFound = true
+		// If we find the section tag
+		if node.Type == html.ElementNode && node.Data == effectiveGoSectionTag {
+			// Add the previous section to the slice of sections
+			sections = append(sections, *section)
+
+			sectionTitle := node.FirstChild.Data
+			sectionFilename = titleToFilename(sectionTitle)
+
+			// Start a new section
+			section = &epubSection{
+				filename: sectionFilename,
+				title:    sectionTitle,
+			}
+		}
+
+		section.nodes = append(section.nodes, *node)
+
+		// Map internal links to their section filename
+		for _, idNode := range htmlutil.GetAllHtmlNodes(node, "", "id", "") {
+			for _, attr := range idNode.Attr {
+				if attr.Key == "id" {
+					internalLinks[attr.Val] = fmt.Sprintf("%s#%s", sectionFilename, attr.Val)
+				}
+			}
+		}
+	}
+
+	// Make sure the last section gets added
+	sections = append(sections, *section)
+
+	e := epub.NewEpub(effectiveGoTitle)
+	// effectiveGoCoverImgPath, err := filepath.Abs(effectiveGoCoverImg)
+	effectiveGoCoverImgPath, err := e.AddImage(effectiveGoCoverImg, "cover.png")
+	if err != nil {
+		return err
+	}
+	e.SetCover(effectiveGoCoverImgPath, "")
+
+	epubCSSPath, err := e.AddCSS(epubCSSFile, "")
+	if err != nil {
+		return err
+	}
+
+	_, err = e.AddFont(preFontFile, "")
+	if err != nil {
+		return err
+	}
+
+	// Iterate through each section and add it to the EPUB
+	for _, section := range sections {
+		sectionContent := ""
+		for _, sectionNode := range section.nodes {
+			// Fix internal links so they work after splitting page into sections
+			for _, linkNode := range htmlutil.GetAllHtmlNodes(&sectionNode, "a", "", "") {
+				for i, attr := range linkNode.Attr {
+					if attr.Key == "href" && strings.HasPrefix(attr.Val, "#") {
+						linkNode.Attr[i].Val = internalLinks[attr.Val[1:]]
+					}
+				}
+			}
+
+			nodeContent, err := htmlutil.HtmlNodeToString(&sectionNode)
+			if err != nil {
+				return err
+			}
+			sectionContent += nodeContent
+		}
+
+		_, err := e.AddSection(sectionContent, section.title, section.filename, epubCSSPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = e.Write(effectiveGoFilename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getId(n *html.Node) string {
+	for _, attr := range n.Attr {
+		if attr.Key == "id" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func titleToFilename(title string) string {
+	title = strings.ToLower(title)
+	title = strings.Replace(title, " ", "-", -1)
+
+	return fmt.Sprintf("%s.xhtml", title)
 }
